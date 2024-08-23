18a2e588f69ed61531acb8391ace3dda92e0dfd9
AbhijeetJ-512 <jabhijeet301@gmail.com>
2024-08-23 11:12:43 +0530

    Climbing Stairs

---
+++ Q__70.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        int x = 1, y = 1;
+        int temp = 0;
+        for (int i = 0; i < n - 1; ++i)
+        {
+            temp = x;
+            x = x + y;
+            y = temp;
+        }
+        return x;
+    }
+};
